51995bded37536510b72a61f68ddb26d825d88ba
Ali Hosam <87545382+alihosam-dev@users.noreply.github.com>
2024-09-26 20:12:04 -0400

    Part 3 - Ali Parts

--- src/main/java/org/translation/JSONTranslationExample.java
+++ src/main/java/org/translation/JSONTranslationExample.java
@@ -38,9 +38,6 @@ public class JSONTranslationExample {
         return canada.getString("es");
     }
 
-    // TODO Task: Complete the method below to generalize the above to get the country name
-    //            for any country code and language code from sample.json.
-
     /**
      * Returns the name of the country based on the provided country and language codes.
      * @param countryCode the country, as its three-letter code.
@@ -48,13 +45,18 @@ public class JSONTranslationExample {
      * @return the translation of country to the given language or "Country not found" if there is no translation.
      */
     public String getCountryNameTranslation(String countryCode, String languageCode) {
+        for (int i = 0; i < jsonArray.length(); i++) {
+            if (jsonArray.getJSONObject(i).getString("alpha3").equals(countryCode)) {
+                return jsonArray.getJSONObject(i).getString(languageCode);
+            }
+        }
         return "Country not found";
     }
-
     /**
      * Prints the Spanish translation of Canada.
      * @param args not used
      */
+
     public static void main(String[] args) {
         JSONTranslationExample jsonTranslationExample = new JSONTranslationExample();
 
